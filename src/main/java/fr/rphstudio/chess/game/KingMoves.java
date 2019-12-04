d6901afc3b11220633eff996bfca5efc3984fdc7
Madenski <bryan.madenski@edu.itescia.fr>
2019-12-04 16:10:30 +0100

    Queen king knigth

--- src/main/java/fr/rphstudio/chess/game/KingMoves.java
+++ src/main/java/fr/rphstudio/chess/game/KingMoves.java
@@ -6,13 +6,41 @@ public class KingMoves implements IMove {
     @Override
     public List<IChess.ChessPosition> getMoves(IChess.ChessPosition p, Board brd) {
         List <IChess.ChessPosition> possibleMoves = new ArrayList<>();
-        int x1;
+
+
+        Moves.straightMoves(possibleMoves, brd, 1, 0, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 1, 1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 0, 1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -1, 1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -1, 0, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -1, -1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 0, -1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 1, -1, 1, p);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*int x1;
         int y1;
         int x0= p.x;
         int y0= p.y;
         for(x1=x0 -1;x1<=x0+1;x1= x1+1){
             for(y1=y0-1;y1<=y0+1;y1=y1+1){
                 IChess.ChessPosition pp = new IChess.ChessPosition(x1,y1);
+
                 if(Moves.isValidPosition(pp)) {
                     if ( Moves.isEmptyCell(pp, brd)) {
                         possibleMoves.add(pp);
@@ -24,7 +52,7 @@ public class KingMoves implements IMove {
                     }
                 }
             }
-        }
+        }*/
         return possibleMoves;
     }
 }
--- src/main/java/fr/rphstudio/chess/game/KnightMoves.java
+++ src/main/java/fr/rphstudio/chess/game/KnightMoves.java
@@ -12,6 +12,19 @@ public class KnightMoves implements IMove {
     @Override
     public List<ChessPosition> getMoves(ChessPosition p, Board brd) {
         List<ChessPosition> possibleMoves = new ArrayList<>();
+
+        /*Moves.straightMoves(possibleMoves, brd,2, 1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 2, -1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -2, -1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -2, 1, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 1, 2, 1, p);
+        Moves.straightMoves(possibleMoves, brd, 1, -2, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -1, -2, 1, p);
+        Moves.straightMoves(possibleMoves, brd, -1, 2, 1, p);
+
+         */
+
+
         int dx;
         int dy;
         int x1;
@@ -21,21 +34,8 @@ public class KnightMoves implements IMove {
         for (dx = -2; dx <= 2; dx = dx + 1) {
             for (dy = -2; dy <= 2; dy = dy + 1) {
                 if (Math.abs(dx) + Math.abs(dy) == 3) {
-                    // Moves.straightMoves(possibleMoves, brd, dx, dy, 1, p);
-                    x1 = x0 + dx;
-                    y1 = y0 + dy;
-                    IChess.ChessPosition pp = new IChess.ChessPosition(x1, y1);
-                    if (Moves.isValidPosition(pp)) {
-                        if (Moves.isEmptyCell(pp, brd)) {
-                            possibleMoves.add(pp);
-                        } else {
-                            IChess.ChessColor c = brd.chessPiece(pp).getPieceColor();
-                            if (Moves.isSameColor(p, c, brd) == false) {
-                                possibleMoves.add(pp)
-                                ;
-                            }
-                        }
-                    }
+                    Moves.straightMoves(possibleMoves, brd, dx, dy, 1, p);
+
                 }
             }
         }
