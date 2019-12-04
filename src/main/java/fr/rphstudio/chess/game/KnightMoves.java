b568b487700378c5a7bc93fd127729cf769cff29
Madenski <bryan.madenski@edu.itescia.fr>
2019-12-04 14:14:08 +0100

    king

--- src/main/java/fr/rphstudio/chess/game/KnightMoves.java
+++ src/main/java/fr/rphstudio/chess/game/KnightMoves.java
@@ -17,21 +17,62 @@ public class KnightMoves implements IMove {
         List <ChessPosition> possibleMoves = new ArrayList<>();
 
         ChessPosition p1 = new ChessPosition(p.x+1,p.y+2);
-        //if (p1){
-            possibleMoves.add(p1);
-        //}
+        if(Utilities.isValidPosition(p1)) {
+            if ( Utilities.isEmpty(p1, brd)) {
+                possibleMoves.add(p1);
+            } else {
+                ChessColor c = brd.chessPiece(p1).getPieceColor();
+                if (Utilities.isSameColor(p, c, brd) == false) {
+                    possibleMoves.add(p1);
 
-/*
-        ChessPosition p2;
-        p2.x = p.x +2;
-        p2.y = p.y +1;
+                }
+
+            }
+        }
+        ChessPosition p2 = new ChessPosition(p.x+2,p.y+1);
+        if(Utilities.isValidPosition(p2)) {
+            if ( Utilities.isEmpty(p2, brd)) {
+                possibleMoves.add(p2);
+            } else {
+                ChessColor c = brd.chessPiece(p2).getPieceColor();
+                if (Utilities.isSameColor(p, c, brd) == false) {
+                    possibleMoves.add(p2);
+
+                }
+
+            }
+        }
 
 
-        ChessPosition p3;
-        p3.x = p.x +2;
-        p3.y = p.y -1;
 
-        ChessPosition p4;
+        ChessPosition p3 = new ChessPosition(p.x+2,p.y-1);
+        if(Utilities.isValidPosition(p3)) {
+            if ( Utilities.isEmpty(p3, brd)) {
+                possibleMoves.add(p3);
+            } else {
+                ChessColor c = brd.chessPiece(p3).getPieceColor();
+                if (Utilities.isSameColor(p, c, brd) == false) {
+                    possibleMoves.add(p3);
+
+                }
+
+            }
+        }
+
+        ChessPosition p4 = new ChessPosition(p.x+1,p.y-2);
+        if(Utilities.isValidPosition(p4)) {
+            if ( Utilities.isEmpty(p4, brd)) {
+                possibleMoves.add(p4);
+            } else {
+                ChessColor c = brd.chessPiece(p4).getPieceColor();
+                if (Utilities.isSameColor(p, c, brd) == false) {
+                    possibleMoves.add(p4);
+
+                }
+
+            }
+        }
+    /*  ChessPosition p4;
         p4.x = p.x +1;
         p4.y = p.y -2;
 
@@ -48,12 +89,22 @@ public class KnightMoves implements IMove {
         p7.x = p.x -2;
         p7.y = p.y +1;
 
+     */
 
-        ChessPosition p8;
-        p8.x = p.x -1;
-        p8.y = p.y +2;
+        ChessPosition p8 = new ChessPosition(p.x-1,p.y+2);
+        if(Utilities.isValidPosition(p8)) {
+            if ( Utilities.isEmpty(p8, brd)) {
+                possibleMoves.add(p8);
+            } else {
+                ChessColor c = brd.chessPiece(p8).getPieceColor();
+                if (Utilities.isSameColor(p, c, brd) == false) {
+                    possibleMoves.add(p8);
 
- */
+                }
+
+            }
+        }
+
 
 
 
